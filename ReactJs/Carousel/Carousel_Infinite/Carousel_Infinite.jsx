14f9c9fa76305b02bab702dc4e85f6f027e21178
= <=>
2020-03-31 20:38:55 +0200

    Carousel Infinite

---
+++ ReactJs/Carousel/Carousel_Infinite/Carousel_Infinite.jsx
@@ -0,0 +1,126 @@
+import React from "react";
+import "./Style.css";
+
+const Animated_Button = () => {
+  return (
+    <div className="carousel">
+      <div className="container">
+        <div className="slide_carousel">
+          <div class="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+          <div class="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+          <div class="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+          <div class="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+          <div class="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+          <div class="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+          <div class="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+          <div class="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+          <div class="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+          <div class="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+          <div class="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+          <div class="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+          <div class="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+          <div class="slide">
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png"
+              height="100"
+              width="250"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Animated_Button;
