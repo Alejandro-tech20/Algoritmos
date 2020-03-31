c8205be16a4427a616511ee7f9dd9d5101b309d2
= <=>
2020-03-31 18:55:14 +0200

    Animated Button with scale

---
+++ ReactJs/Animaciones/Hover/Animated_Button/Animated_Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Style.css'
+
+const Animated_Button = ()=>{
+    return(
+        <div className='para_button'>
+            <button>
+                Enviar
+            </button>
+        </div>
+    )
+}
+export default Animated_Button;
