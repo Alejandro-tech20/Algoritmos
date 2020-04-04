e7d2b9f35e5c9d0c4de139b4504634db59e9bf20
= <=>
2020-04-04 10:06:35 +0200

    Animation de Reloj de Arena

---
+++ ReactJs/Animaciones/Loading_Reloj/Loading_Reloj.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Screen1.css";
+import nyorck from "../../img/new_yorck.jpg";
+import TodoList from "../Animated_Item/AnimatedItem";
+
+const Screen1 = () => {
+  return (
+    <div className="screen1">
+      <div className="screen1_sub">
+      <div className="container">
+        <div className="container_box">
+
+        </div>
+      </div>
+      </div>
+      
+    </div>
+  );
+};
+export default Screen1;
