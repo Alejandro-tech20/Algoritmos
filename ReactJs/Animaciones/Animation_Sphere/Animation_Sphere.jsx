6e97888a8e5a100784d228c164d59992795df57b
= <=>
2020-04-09 11:20:32 +0200

    animation with only scss of sphere rotated

---
+++ ReactJs/Animaciones/Animation_Sphere/Animation_Sphere.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./sphere.css";
+
+const Sphere = () => {
+  return (
+    <div>
+      <div className="container_dos">
+        <div className="view">
+          <div className="plane main">
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Sphere;
